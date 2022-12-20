ab1cd5c443f767c1a06beb038de3aa3ddf9989df
Yashvardhan sharma <95120981+Yvsharma@users.noreply.github.com>
2022-12-20 14:49:53 +0530

    Basic SQL
    Theory part only

---
+++ Basic theory.sql
@@ -0,0 +1,29 @@
+SQL basic
+
+DATABASE :- It is a collection of data and holds this data in the form of Table.
+
+Table:- Holds the data in form of rows and column.
+it is similar to Excel spreadsheet.
+
+The database provides us the capability to access and manipulate this data.
+
+
+Kind of database we have:-
+1)Relational data base :- Stored in the form of Raq and column and also the table have relation betwen them.
+EX. :-
+MySQL
+SQL Server
+PostreSQL
+MariaDB
+
+2)No-relational database/NOSQL database :- Key value,Document,Graph
+EX. :-
+Hbase
+mongoDB
+cassandra
+
+SQL :- Structured Query Language and is used to query a relational DB(Database).
+
+:- MySQL vs SQL :- NoRelational vs relational
+
+
